3844dc816bdb2af1487591ac69903560f6895d5b
Italo-Diass <italogabrielsiqueira02@gmail.com>
2024-07-17 22:43:47 -0300

    usefel_methods_for_objects

---
+++ useful_methods_for_objects.js
@@ -0,0 +1,27 @@
+/*
+Object.values
+Object.entries
+Object.assign(des, any)
+Object.getOwnPropertyDescriptor(o, 'prop')
+...(spread)
+
+Object.keys(retorna as chaves)
+Object.freeze(congela o objeto)
+Object.defineProperties(define várias propriedades)
+Object.defineProperty(define uma propriedade)
+*/
+
+
+/* const produto = {nome: 'Caneca', preco: 1.8};
+const caneca = {
+    ...produto,
+    material: 'porcelana'
+};
+
+caneca.nome =  'Outro nome';
+caneca.preco = 2.5;
+
+console.log(produto);
+console.log(caneca); */
+
+
