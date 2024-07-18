0b49d169ac8a280a68f26fb30fe80fb9b2c6bc8e
Italo-Diass <italogabrielsiqueira02@gmail.com>
2024-07-18 18:19:16 -0300

    useful_methods_for_objects

--- useful_methods_for_objects.js
+++ useful_methods_for_objects.js
@@ -11,17 +11,8 @@ Object.defineProperties(define várias propriedades)
 Object.defineProperty(define uma propriedade)
 */
 
+const produto = {nome: 'Produto', preco: 1.8, material: 'porcelana'};
 
-/* const produto = {nome: 'Caneca', preco: 1.8};
-const caneca = {
-    ...produto,
-    material: 'porcelana'
-};
-
-caneca.nome =  'Outro nome';
-caneca.preco = 2.5;
-
-console.log(produto);
-console.log(caneca); */
-
-
+for (let valor of Object.entries(produto)) {
+    console.log(valor[0], valor[1])
+}
