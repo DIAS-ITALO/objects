b304abf9e4c8ebc9dbd192cde6b64b8f5a5f87be
Italo-Diass <italogabrielsiqueira02@gmail.com>
2024-07-22 11:35:33 -0300

    manipulating_prototypes

--- manipulating_prototypes.js
+++ manipulating_prototypes.js
@@ -9,10 +9,31 @@ Produto.prototype.desconto = function(percentual) {
 };
 
 Produto.prototype.aumento = function(percentual) {
-    this.preco = this.preco + (this.preco * (percentual / 100));
+  this.preco = this.preco + (this.preco * (percentual / 100));
 };
 
 const p1 = new Produto('Camiseta', 50);
-/* p1.desconto(100);
- */p1.aumento(100);
-console.log(p1);
+
+const p2 = {
+    nome: 'Caneca',
+    preco: 15
+}
+Object.setPrototypeOf(p2, Produto.prototype)
+
+const p3 = Object.create(Produto.prototype, {
+    preco: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 99
+    },
+    tamanho: {
+        writable: true, 
+        configurable: true,
+        enumerable: true,
+        value: 42
+    },
+});
+p3.aumento(10);
+console.log(p3);
+
